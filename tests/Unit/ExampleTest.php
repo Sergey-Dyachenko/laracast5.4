577404c3297d6d8cb49df53052b982f932450f63
serhio <serjdyachenko@gmail.com>
2017-12-29 18:46:55 +0200

    lesson 22

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -3,7 +3,9 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
+use App\Post;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+
 
 class ExampleTest extends TestCase
 {
@@ -14,6 +16,20 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+        //Give I have two records in the database that are posts
+
+        //and each one is posted a month apart
+        $first = factory(Post::class)->create();
+        $second = factory(Post::class)->create([
+            'created_at' => \Carbon\Carbon::now()->subMonth()
+        ]);
+
+        //When I fetch the archives
+        $posts = Post::archives();
+
+        //THen the response should be in the proper format
+
+        $this->assertCount(2, $posts);
+
     }
 }
